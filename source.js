2079c362142b4726d9126ca4d90ec844a84bb337
IlfyPlorious <sdragosflorian@gmail.com>
2021-06-09 18:51:40 +0300

    first push

---
+++ source.js
@@ -0,0 +1,11 @@
+const menuBtn = document.getElementById('mobileMenuIcon')
+      nav = document.querySelector('nav')
+      menuBtnExit = document.getElementById('mobileXIcon');
+
+menuBtn.addEventListener('click', () => {
+    nav.classList.add('menuBtn');
+})
+
+menuBtnExit.addEventListener('click', () => {
+    nav.classList.remove('menuBtn');
+})
